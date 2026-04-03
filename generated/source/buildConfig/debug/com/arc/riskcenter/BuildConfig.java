caa833aecc8fed831792352a69713aa2ba7fd190
Genius-d <68971274+Genius-d@users.noreply.github.com>
2026-04-04 00:47:00 +0530

    Add files via upload

---
+++ generated/source/buildConfig/debug/com/arc/riskcenter/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.arc.riskcenter;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.arc.riskcenter";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+  // Field from default config.
+  public static final String ARC_API_KEY = "arc-hacksagon-2026";
+  // Field from default config.
+  public static final String BASE_URL = "http://localhost:8000";
+  // Field from default config.
+  public static final String WS_URL = "ws://localhost:8000/ws/logs";
+}
